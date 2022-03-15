52125571e6c433ca18285b6266159db5803e0a6f
Poojan <poojanpranshu@gmail.com>
2022-03-15 15:19:17 -0300

    I have added the for loop in the file

--- mygitdemo/src/mygitdemo/version.java
+++ mygitdemo/src/mygitdemo/version.java
@@ -2,12 +2,14 @@ package mygitdemo;
 
 public class version {
 
-	
-	
-
-		public static void main(String[] args)
-		{
-			System.out.println("Java is owned by Oracle");
-		}	
+	public static void main(String[] args) {
+		System.out.println("Java is owned by Oracle");
+		
+		
+		for(int i=1;i<=9;i++) {
+			
+			System.out.println("The number is " + i);
+		}
+		
 	}
-
+}
